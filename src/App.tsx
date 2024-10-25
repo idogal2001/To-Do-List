aa796df62d20ef434bf19d47ed49199f79b39ec6
idogal2001 <idogal2001@gmail.com>
2024-10-25 15:48:56 +0300

    ToDoListProject

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+import logo from './logo.svg';
+import Tasks from './components/Tasks';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+        <Tasks />
+    </main>
+  );
+}
+
+export default App;
